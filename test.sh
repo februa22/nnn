6a9f6c1caa8208fb06390e937fdf046de55a3d2f
BYUNG WOO LEE <bart2001@naver.com>
2018-11-07 14:57:27 +0900

    post_tagger_tester path edited

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 ANSWER_FILE=usr_dir/t2t_decode/sejong_raw_refine_tokenize_output.txt
 OUTPUT_FILE=usr_dir/t2t_decode/sejong_raw_refine_tokenize_decoded.txt
 
-python -u pos_tagger_tester.py \
+python -u pos_tagger/pos_tagger_tester.py \
     --answer_file=$ANSWER_FILE \
     --output_file=$OUTPUT_FILE
